13078dcb807f441402dd0fd3c14a2584adc82e86
nomayen <nomayen.ohin@gmail.com>
2023-01-05 23:34:17 +0600

    push code

--- index.js
+++ index.js
@@ -1,43 +1,54 @@
-const express = require('express')
-const cors = require('cors')
+const express =require('express');
+const cors =require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
-const port = process.env.PORT || 5000;
+const port =process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
 
-// name :dbuser2
-// password: oQcL0s6KU6IAlVOx
+
+// user : admin
+// pass : admin
 
 
 
-
-const uri = "mongodb+srv://dbuser2:oQcL0s6KU6IAlVOx@cluster0.07lgbsy.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://admin:admin@cluster0.07lgbsy.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run() {
-    try {
-        const userCollection = client.db('nodeMongoCrud').collection('users');
 
-        const user = {
-            name:'testing test',
-            email:'testing@gmail.com'
+async function run() {
+    try{
+        const userCollection =client.db('nodeMongoCruds').collection('users');
+        const user={
+            name: 'testing test',
+            email: 'testingtest@gmail.com'
         }
         const result =await userCollection.insertOne(user);
         console.log(result);
     }
-    finally {
-//              11-50-----------------------------
+    finally{
+
     }
 }
 
-run().catch(err => console.log(err));
 
-app.get('/', (req, res) => {
-    res.send('Hello from nodemongo crud server')
+run().catch(err =>console.log(err));
+
+
+
+
+app.get('/',(req, res)=>{
+    res.send('Hello from node mongo');
+});
+
+app.listen(port, () =>{
+    console.log(`listening to port ${port}`);
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-})
+
+
+
+
+
